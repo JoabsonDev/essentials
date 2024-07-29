2f23c06c55fbc343d56d9b70815244a9a12211c4
joabson_ <joabsonguitar@gmail.com>
2024-07-29 09:16:31 -0300

    refactor: para paradigma de classes

--- js/script.js
+++ js/script.js
@@ -1,116 +1,144 @@
-const links = document.querySelectorAll("[data-link]")
-const refs = [...links].map(({ href }) => {
-  const [_, id] = href.split("#")
+// Helper Functions
+const getElement = (selector) => document.querySelector(selector)
+const getElements = (selector) => document.querySelectorAll(selector)
 
-  return document.querySelector(`#${id}`)
-})
-
-// header
-const header = document.querySelector("#header")
-function headerControl() {
-  let current = ""
-
-  if (window.scrollY >= 400 && header) {
-    header.classList.add("fixed")
-    header.classList.add("bg-white")
-    header.classList.add("border-b")
-    header.classList.add("border-gray-200")
-    header.classList.remove("absolute")
-  } else {
-    header.classList.remove("fixed")
-    header.classList.remove("bg-white")
-    header.classList.remove("border-b")
-    header.classList.remove("border-gray-200")
-    header.classList.add("absolute")
-  }
-
-  refs.forEach((ref) => {
-    const sectionTop = ref.offsetTop
-    const sectionHeight = ref.clientHeight
-    if (pageYOffset >= sectionTop - sectionHeight / 3) {
-      current = ref.getAttribute("id")
-    }
-  })
-
-  links.forEach((link) => {
-    link.classList.remove("text-blue-500")
-    link.classList.add("text-gray-700")
-
-    if (link.getAttribute("href") === "#" + current) {
-      link.classList.add("text-blue-500")
-      link.classList.remove("text-gray-700")
-    }
-  })
-}
-headerControl()
-document.addEventListener("scroll", headerControl)
-
-// menu header
-const menuToggle = document.querySelector("#menu-toggle")
-const menu = document.querySelector("#menu")
-
-menuToggle.addEventListener("click", () => {
-  menu.classList.toggle("hidden")
-})
-
-// modal
-const showModalButton = document.querySelector("#show-modal")
-const closeModalButton = document.querySelector("#close-modal")
-const dialog = document.querySelector(".dialog")
-
-showModalButton.addEventListener("click", () => {
-  dialog.showModal()
-  player = document.querySelector(".video-stream")
-})
-
-closeModalButton.addEventListener("click", () => {
-  dialog.close()
-
-  const iframes = document.querySelectorAll("iframe")
-  if (iframes.length > 0)
-    for (const iframe of iframes) {
-      const src = iframe.src
-      iframe.src = ""
-
-      const timeoutId = setTimeout(() => {
-        iframe.src = src
-        clearTimeout(timeoutId)
-      }, 200)
-    }
-})
-
-// accordion
-const accordionItemsSelector = '[data-id="accordion"]  input[type="checkbox"]'
-const accordionItems = document.querySelectorAll(accordionItemsSelector)
-
-for (const accordionItem of accordionItems)
-  accordionItem.addEventListener("change", () => {
-    accordionItems.forEach((item) => {
-      if (item !== accordionItem) item.checked = false
+// Header Control
+class HeaderControl {
+  constructor() {
+    this.header = getElement("#header")
+    this.links = Array.from(getElements("[data-link]"))
+    this.refs = this.links.map(({ href }) => {
+      const [, id] = href.split("#")
+      return getElement(`#${id}`)
     })
-  })
-
-// TODO: terminar a lógica do carousel para ser dinâmico
-// carousel
-class Carousel {
-  _element = null
-  _currentIndex = 0
-  _state = { active: "opacity-100", inactive: "opacity-50" }
-
-  constructor({ selector = '[data-id="carousel"]', state }) {
-    const element = document.querySelector(selector)
-    if (!element) return
-
-    this._element = element
-    this._state = {
-      ...this._state,
-      ...state
-    }
-
     this.init()
   }
 
   init() {
+    this.updateHeader()
+    document.addEventListener("scroll", this.updateHeader.bind(this))
+  }
+
+  updateHeader() {
+    const scrollY = window.scrollY
+    const headerClasses = ["fixed", "bg-white", "border-b", "border-gray-200"]
+    const headerClassToRemove = "absolute"
+
+    if (scrollY >= 400 && this.header) {
+      this.header.classList.add(...headerClasses)
+      this.header.classList.remove(headerClassToRemove)
+    } else {
+      this.header.classList.remove(...headerClasses)
+      this.header.classList.add(headerClassToRemove)
+    }
+
+    let current = ""
+    for (const ref of this.refs) {
+      const sectionTop = ref.offsetTop
+      const sectionHeight = ref.clientHeight
+      if (scrollY >= sectionTop - sectionHeight / 3) {
+        current = ref.getAttribute("id")
+      }
+    }
+
+    this.links.forEach((link) => {
+      link.classList.remove("text-blue-500")
+      link.classList.add("text-gray-700")
+      if (link.getAttribute("href") === `#${current}`) {
+        link.classList.add("text-blue-500")
+        link.classList.remove("text-gray-700")
+      }
+    })
+  }
+}
+
+// Menu Toggle
+class MenuToggle {
+  constructor() {
+    this.menuToggle = getElement("#menu-toggle")
+    this.menu = getElement("#menu")
+    this.init()
+  }
+
+  init() {
+    this.menuToggle.addEventListener("click", () => {
+      this.menu.classList.toggle("hidden")
+    })
+  }
+}
+
+// Modal Control
+class ModalControl {
+  constructor() {
+    this.showModalButton = getElement("#show-modal")
+    this.closeModalButton = getElement("#close-modal")
+    this.dialog = getElement(".dialog")
+    this.init()
+  }
+
+  init() {
+    this.showModalButton.addEventListener("click", () =>
+      this.dialog.showModal()
+    )
+    this.closeModalButton.addEventListener("click", this.closeModal.bind(this))
+  }
+
+  closeModal() {
+    this.dialog.close()
+    const iframes = getElements("iframe")
+    if (iframes.length > 0) {
+      iframes.forEach((iframe) => {
+        const src = iframe.src
+        iframe.src = ""
+        setTimeout(() => {
+          iframe.src = src
+        }, 200)
+      })
+    }
+  }
+}
+
+// Accordion Control
+class AccordionControl {
+  constructor() {
+    this.accordionItems = getElements(
+      '[data-id="accordion"]  input[type="checkbox"]'
+    )
+    this.init()
+  }
+
+  init() {
+    this.accordionItems.forEach((item) => {
+      item.addEventListener("change", () => {
+        this.accordionItems.forEach((i) => {
+          if (i !== item) i.checked = false
+        })
+      })
+    })
+  }
+}
+
+// Carousel
+class Carousel {
+  constructor({ selector = '[data-id="carousel"]', state = {} }) {
+    this.element = getElement(selector)
+    if (!this.element) return
+
+    this.state = {
+      active: "opacity-100",
+      inactive: "opacity-50",
+      ...state
+    }
+    this._currentIndex = 0
+    this.init()
+  }
+
+  init() {
+    this.bindEvents()
+    setInterval(() => this.next(), 10000)
+  }
+
+  bindEvents() {
     this.actions.forEach((action, index) =>
       action.addEventListener("click", () => {
         this._currentIndex = index
@@ -119,24 +147,17 @@ class Carousel {
     )
 
     this.previous?.addEventListener("click", () => {
-      this._currentIndex -= 1
-
-      if (this._currentIndex < 0) this._currentIndex = this.items.length - 1
-
+      this._currentIndex =
+        (this._currentIndex - 1 + this.items.length) % this.items.length
       this.control()
     })
 
-    const next = () => {
-      this._currentIndex += 1
+    this.next?.addEventListener("click", () => this.next())
+  }
 
-      if (this._currentIndex >= this.items.length) this._currentIndex = 0
-
-      this.control()
-    }
-
-    this.next?.addEventListener("click", next)
-
-    setInterval(next, 10000)
+  next() {
+    this._currentIndex = (this._currentIndex + 1) % this.items.length
+    this.control()
   }
 
   control() {
@@ -149,32 +170,59 @@ class Carousel {
       item.style.transform = `translateX(${translateX}%)`
     })
     this.actions.forEach((btn, btnIndex) => {
-      if (btnIndex === this._currentIndex) {
-        btn.classList.remove(this._state.inactive)
-        btn.classList.add(this._state.active)
-      } else {
-        btn.classList.add(this._state.inactive)
-        btn.classList.remove(this._state.active)
-      }
+      btn.classList.toggle(this.state.active, btnIndex === this._currentIndex)
+      btn.classList.toggle(this.state.inactive, btnIndex !== this._currentIndex)
     })
   }
 
   get items() {
-    return this._element.querySelectorAll('[data-id="carousel-item"]')
+    return this.element.querySelectorAll('[data-id="carousel-item"]')
   }
 
   get actions() {
-    return this._element.querySelectorAll('[data-id="carousel-action"]')
+    return this.element.querySelectorAll('[data-id="carousel-action"]')
   }
 
   get previous() {
-    return this._element.querySelector('[data-id="carousel-previous"]')
+    return this.element.querySelector('[data-id="carousel-previous"]')
   }
 
   get next() {
-    return this._element.querySelector('[data-id="carousel-next"]')
+    return this.element.querySelector('[data-id="carousel-next"]')
   }
 }
+
+// Form Control
+class FormControl {
+  constructor({
+    selectorForm = "",
+    selectorMessage = "",
+    classToggle = "hidden"
+  }) {
+    this.formElement = getElement(selectorForm)
+    this.formMessage = getElement(selectorMessage)
+    this.classToggle = classToggle
+    this.init()
+  }
+
+  init() {
+    this.formElement.addEventListener("submit", (event) => {
+      event.preventDefault()
+      if (this.formElement.checkValidity()) {
+        setTimeout(() => {
+          this.formElement.classList.add(this.classToggle)
+          this.formMessage.classList.remove(this.classToggle)
+        }, 1000)
+      }
+    })
+  }
+}
+
+// Instantiate Classes
+new HeaderControl()
+new MenuToggle()
+new ModalControl()
+new AccordionControl()
 
 new Carousel({
   selector: "#showcase"
@@ -188,26 +236,11 @@ new Carousel({
   }
 })
 
-const waitingListForm = document.querySelector("#waiting-list")
-const waitingListMessage = document.querySelector("#waiting-list-message")
-waitingListForm.addEventListener("submit", (event) => {
-  event.preventDefault()
-
-  const timeoutId = setTimeout(() => {
-    waitingListForm.classList.add("hidden")
-    waitingListMessage.classList.remove("hidden")
-    clearTimeout(timeoutId)
-  }, 1000)
+new FormControl({
+  selectorForm: "#waiting-list",
+  selectorMessage: "#waiting-list-message"
 })
-
-const contactForm = document.querySelector("#contact-form")
-const contactMessage = document.querySelector("#contact-form-message")
-contactForm.addEventListener("submit", (event) => {
-  event.preventDefault()
-
-  const timeoutId = setTimeout(() => {
-    contactForm.classList.add("hidden")
-    contactMessage.classList.remove("hidden")
-    clearTimeout(timeoutId)
-  }, 1000)
+new FormControl({
+  selectorForm: "#contact-form",
+  selectorMessage: "#contact-form-message"
 })
